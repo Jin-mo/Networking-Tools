ae3f5f6ddc740dd1abeff8edc2953b08c05a8172
Jin-mo <wlone825@gmail.com>
2023-12-20 19:05:54 +0300

    nearly done

--- Netcat.py
+++ Netcat.py
@@ -16,7 +16,24 @@ def execute(cmd):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='BHP Net Tool', formatter_class=argparse.RawDescriptionHelpFormatter, 
-    epilog=textwrap.dedent('Example : 
+    epilog=textwrap.dedent('''Example : 
     netcat.py -t 192.168.1.108 -p 5555 -l -c # command shell
     netcat.py -t 192.168.1.108 -p 5555 -l -u=mytest.txt #upload to file
-    ))
+    netcat.py -t 192.168.1.108 -p 5555 -l -e=\\"cat /etc/passwd\\ # execute command
+    echo 'ABC' | ./netcat.py -t 192.168.1.108 -p 135 # echo text to server port 135
+    netcat.py -t 192.168.1.108 -p 5555 # connect to server 
+    '''))
+
+parser.add_argument('-c', '--command', action='store_true', help='command shell')
+parser.add_argument('-e', '--execute', help='execute specified command')
+parser.add_argument('-l', '--listen', action='store_true', help='liste')
+parser.add_argument('-p', '--port', type=int, default=5555, help='specified port')
+parser.add_argument('-t', 'target', default='192.168.1.108', help='specified IP')
+args = parser.parse_args()
+if args.liste:
+    buffer = ''
+else:
+    buffer = sys.stdin.read()
+
+nc = NetCat(args, buffer.encode())
+nc.run
